4dd86a193e0207fa8923b3bb3a04d919562df8c1
Merouane Atig <merouane.atig@gmail.com>
2014-12-11 17:03:56 +0100

    Add paddle object

--- pong.rb
+++ pong.rb
@@ -1,12 +1,41 @@
 require 'gosu'
 
+class Paddle
+  def initialize(window, x)
+    @window = window
+    @height = 100
+    @width = 10
+    @x = x
+    @y = @window.height / 2 - @height / 2
+    @color = Gosu::Color::RED
+  end
+
+  def update
+    @y -= 5 if @window.button_down? Gosu::KbUp
+    @y += 5 if @window.button_down? Gosu::KbDown
+  end
+
+  def draw
+    @window.draw_quad(@x, @y, @color, @x, @y + @height, @color, @x + @width, @y + @height, @color, @x + @width, @y, @color)
+  end
+end
+
 class PongGame < Gosu::Window
   def initialize(width=800, height=600, fullscreen=false)
     super
+    @paddle1 = Paddle.new(self, 10)
   end
 
   def button_down(id)
     self.close if id == Gosu::KbEscape
+  end
+
+  def update
+    @paddle1.update
+  end
+
+  def draw
+    @paddle1.draw
   end
 end
 

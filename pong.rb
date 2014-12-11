e3f3bdc9afe5d9cc2d8df54e95bf3d7096d06641
Merouane Atig <merouane.atig@gmail.com>
2014-12-11 16:35:37 +0100

    Close window when pressing escape

--- pong.rb
+++ pong.rb
@@ -4,6 +4,10 @@ class PongGame < Gosu::Window
   def initialize(width=800, height=600, fullscreen=false)
     super
   end
+
+  def button_down(id)
+    self.close if id == Gosu::KbEscape
+  end
 end
 
 PongGame.new.show

063bbd262cddbd8b3b4b60f36867fa4804f3d509
Merouane Atig <merouane.atig@gmail.com>
2014-12-11 17:07:02 +0100

    Add second paddle

--- pong.rb
+++ pong.rb
@@ -1,18 +1,20 @@
 require 'gosu'
 
 class Paddle
-  def initialize(window, x)
+  def initialize(window, x, key_up, key_down)
     @window = window
     @height = 100
     @width = 10
     @x = x
     @y = @window.height / 2 - @height / 2
     @color = Gosu::Color::RED
+    @key_up = key_up
+    @key_down = key_down
   end
 
   def update
-    @y -= 5 if @window.button_down? Gosu::KbUp
-    @y += 5 if @window.button_down? Gosu::KbDown
+    @y -= 5 if @window.button_down? @key_up
+    @y += 5 if @window.button_down? @key_down
   end
 
   def draw
@@ -23,7 +25,8 @@ end
 class PongGame < Gosu::Window
   def initialize(width=800, height=600, fullscreen=false)
     super
-    @paddle1 = Paddle.new(self, 10)
+    @paddle1 = Paddle.new(self, 10, Gosu::KbE, Gosu::KbD)
+    @paddle2 = Paddle.new(self, width - 20, Gosu::KbUp, Gosu::KbDown)
   end
 
   def button_down(id)
@@ -32,10 +35,12 @@ class PongGame < Gosu::Window
 
   def update
     @paddle1.update
+    @paddle2.update
   end
 
   def draw
     @paddle1.draw
+    @paddle2.draw
   end
 end
 
